24c841e3d640c9d1cdfeb14c6c3d218f5677b7a8
Melanie <melaine.santos@ba.estudante.senai.br>
2023-09-22 17:19:11 -0300

    ATT - 22/09

---
+++ app.js
@@ -0,0 +1,23 @@
+//Inicializando express
+const express = require('express')
+const app = express()
+
+//Configurando handlebars
+const handlebars = require('express-handlebars')
+
+app.engine('handlebars',handlebars.engine({defaultLayout:'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/sobrenos',function(req,res){
+    res.render('pages/sobrenos.handlebars')
+})
+
+app.get('/', (req, res) => res.render('pages/index.handlebars'));
+
+
+app.listen(8080,function(){
+    
+    console.log("Servidor iniciado na url http://localhost:8080")
+})

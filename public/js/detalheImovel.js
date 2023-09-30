e7013c358fce78929a4a09dda243bd6c397ce8e7
Melanie <melaine.santos@ba.estudante.senai.br>
2023-09-30 12:02:36 -0300

    Refatora função abrirMapa

--- public/js/detalheImovel.js
+++ public/js/detalheImovel.js
@@ -1,3 +1,5 @@
+let aux = [];
+
 function abrirMapa() {
     const mapa = document.getElementById('mapa');
     const areaMapa = document.getElementById('area-mapa')
@@ -6,9 +8,17 @@ function abrirMapa() {
     mapa.style.display = "block"
     setTimeout(()=>{
         areaMapa.style.width = "50%";
+        aux[0] = 1;
+
     },2)
 
     blur.addEventListener('click',()=>{
         mapa.style.display = "none";
+        aux[1] = 2;
     })
+
+
+    if (aux[0] === 1 && aux[1] === 2) {
+        areaMapa.style.width = "0%";
+    }
 }

74014e21373c1bb477e9871eb562f8cdfb724811
Melanie <melaine.santos@ba.estudante.senai.br>
2023-10-03 17:11:14 -0300

    Adiciona modal mais imagens

--- public/js/detalheImovel.js
+++ public/js/detalheImovel.js
@@ -1,19 +1,20 @@
 let aux = [];
 
 function abrirMapa() {
-    const mapa = document.getElementById('mapa');
+    const mapa = document.getElementById('mapaContainer');
     const areaMapa = document.getElementById('area-mapa')
-    const blur = document.getElementById('blur')
 
     mapa.style.display = "block"
     setTimeout(()=>{
         areaMapa.style.width = "50%";
         aux[0] = 1;
 
-    },2)
+},2)
 
-    blur.addEventListener('click',()=>{
-        mapa.style.display = "none";
+    window.addEventListener('click',(event)=>{
+        if(event.target == mapaContainer){
+            mapa.style.display = "none";
+        }
         aux[1] = 2;
     })
 
@@ -21,4 +22,38 @@ function abrirMapa() {
     if (aux[0] === 1 && aux[1] === 2) {
         areaMapa.style.width = "0%";
     }
+}
+
+function verImagens(){
+    const containerModal = document.getElementById('modalImagens')
+
+    containerModal.style.display = "flex"
+    window.addEventListener('click',(event)=>{
+        if(event.target == modalImagens){
+            containerModal.style.display = "none";
+        }
+    })
+}
+
+
+
+
+var posicaoSlide = 1;
+showSlide(posicaoSlide);
+
+function add(n) {
+  showSlide(posicaoSlide += n);
+}
+
+function showSlide(n) {
+  var i;
+  var slide = document.getElementsByClassName("bloco");
+
+  if (n > slide.length) {posicaoSlide = 1}
+  if (n < 1) {posicaoSlide = slide.length}
+  
+  for (i = 0; i < slide.length; i++) {
+    slide[i].style.display = "none";
+  }
+  slide[posicaoSlide-1].style.display = "flex";
 }

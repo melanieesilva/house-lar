8c438b0172b7f65f0becc101c83ebb483072faf0
Melanie <melaine.santos@ba.estudante.senai.br>
2023-10-04 20:26:30 -0300

    Adiciona slide gallery em Detalhe Imóvel

--- public/js/detalheImovel.js
+++ public/js/detalheImovel.js
@@ -5,14 +5,14 @@ function abrirMapa() {
     const areaMapa = document.getElementById('area-mapa')
 
     mapa.style.display = "block"
-    setTimeout(()=>{
+    setTimeout(() => {
         areaMapa.style.width = "50%";
         aux[0] = 1;
 
-},2)
+    }, 2)
 
-    window.addEventListener('click',(event)=>{
-        if(event.target == mapaContainer){
+    window.addEventListener('click', (event) => {
+        if (event.target == mapaContainer) {
             mapa.style.display = "none";
         }
         aux[1] = 2;
@@ -24,36 +24,90 @@ function abrirMapa() {
     }
 }
 
-function verImagens(){
+function verImagens() {
     const containerModal = document.getElementById('modalImagens')
 
     containerModal.style.display = "flex"
-    window.addEventListener('click',(event)=>{
-        if(event.target == modalImagens){
+    window.addEventListener('click', (event) => {
+        if (event.target == modalImagens) {
             containerModal.style.display = "none";
         }
     })
 }
 
 
+const botaoCarrossel = document.getElementById('botoesControl');
+const botaoSlide = document.getElementById('botoesSlide');
+const divCarrossel = document.querySelector('blocoImagens')
 
 
+
+// divCarrossel.forEach(div =>{
+//     div.addEventListener('click',console.log("olaaa"))
+// })
+
+divCarrossel.addEventListener('click',({ target })=>{
+    target.addEventListener('click',() => {
+        console.log("oi");
+    })
+
+})
+
+
+
+var posicaoCarrossel = 1;
 var posicaoSlide = 1;
+showCarrossel(posicaoCarrossel);
 showSlide(posicaoSlide);
 
-function add(n) {
-  showSlide(posicaoSlide += n);
+function addCarrossel(n) {
+    showCarrossel(posicaoCarrossel += n);
 }
 
-function showSlide(n) {
-  var i;
-  var slide = document.getElementsByClassName("bloco");
+function addSlide(n){
+    showSlide(posicaoSlide += n);
+}
 
-  if (n > slide.length) {posicaoSlide = 1}
-  if (n < 1) {posicaoSlide = slide.length}
-  
-  for (i = 0; i < slide.length; i++) {
-    slide[i].style.display = "none";
-  }
-  slide[posicaoSlide-1].style.display = "flex";
+function slideAtual(n){
+    showSlide(posicaoSlide = n)
+}
+
+function showCarrossel(n) {
+    var i;
+    var bloco = document.getElementsByClassName("bloco-imagens");
+
+    if (n > bloco.length) { posicaoCarrossel = 1 }
+    if (n < 1) { posicaoCarrossel = bloco.length }
+
+    for (i = 0; i < bloco.length; i++) {
+        bloco[i].style.display = "none";
+    }
+
+
+    if (posicaoCarrossel === 1) {
+        botaoCarrossel.children[0].style.display = "none";
+    }else{
+        botaoCarrossel.children[0].style.display = "flex";
+    }
+    bloco[posicaoCarrossel - 1].style.display = "flex";
+}
+
+function showSlide(n){
+    var i;
+    var slide = document.getElementsByClassName("imagem-slide");
+
+    if (n > slide.length) { posicaoSlide = 1 }
+    if (n < 1) { posicaoSlide = slide.length }
+
+    for (i = 0; i < slide.length; i++) {
+        slide[i].style.display = "none";
+    }
+
+
+    if (posicaoSlide === 1) {
+        botaoSlide.children[0].style.display = "none";
+    }else{
+        botaoSlide.children[0].style.display = "flex";
+    }
+    slide[posicaoSlide - 1].style.display = "flex";
 }

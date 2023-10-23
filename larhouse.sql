2c38192b64ba2ff6fb602c86d7290fdf5b3aebf3
Alexandre Amorim <alexandrepinto2005@hotmail.com>
2023-10-22 21:49:54 -0300

    atualização banco de dados

--- larhouse.sql
+++ larhouse.sql
@@ -28,10 +28,14 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `agenda` (
-  `id_agenda` int(11) NOT NULL,
-  `mes` varchar(50) NOT NULL,
-  `dia` varchar(50) NOT NULL,
-  `horario` varchar(50) NOT NULL
+  `id_agenda` INT(11) NOT NULL,
+  `mes` VARCHAR(50) NOT NULL,
+  `dia` VARCHAR(50) NOT NULL,
+  `horario` VARCHAR(50) NOT NULL,
+  `cliente_id` INT(11),
+  `FK_imovel_id` INT(11),
+  FOREIGN KEY (`cliente_id`) REFERENCES `cliente`(`id_cliente`)/Chave estrangeira que traz o id do cliente para saber quem é o dono do imóvel/
+  FOREIGN KEY (`FK_imovel_id`) REFERENCES `imoveis`(`id_imovel`) /Chave estrangeira que traz o imóvel solicitado para a agenda/
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -41,8 +45,9 @@ CREATE TABLE `agenda` (
 --
 
 CREATE TABLE `categoria_noticia` (
-  `id_categoria` int(11) NOT NULL,
-  `nome_categoria` varchar(200) NOT NULL
+  `id_categoria` INT(11) NOT NULL,
+  `nome_categoria` VARCHAR(200) NOT NULL,
+  `cor_categoria` ENUM('Item 1','Item 2','Item 3')
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -52,10 +57,10 @@ CREATE TABLE `categoria_noticia` (
 --
 
 CREATE TABLE `cliente` (
-  `id_cliente` int(11) NOT NULL,
-  `nome_cliente` varchar(200) NOT NULL,
-  `email_cliente` varchar(200) NOT NULL,
-  `telefone_cliente` varchar(20) NOT NULL
+  `id_cliente` INT(11) NOT NULL,
+  `nome_cliente` VARCHAR(100) NOT NULL,
+  `email_cliente` VARCHAR(100) NOT NULL,
+  `telefone_cliente` VARCHAR(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -65,11 +70,11 @@ CREATE TABLE `cliente` (
 --
 
 CREATE TABLE `duvidas` (
-  `id_duvida` int(11) NOT NULL,
-  `nome_cliente` varchar(200) NOT NULL,
-  `email_cliente` varchar(200) NOT NULL,
-  `telefone_cliente` varchar(20) NOT NULL,
-  `mensagem_cliente` text NOT NULL
+  `id_duvida` INT(11) NOT NULL,
+  `nome_cliente` VARCHAR(200) NOT NULL,
+  `email_cliente` VARCHAR(200) NOT NULL,
+  `telefone_cliente` VARCHAR(20) NOT NULL,
+  `mensagem_cliente` TEXT NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -79,9 +84,12 @@ CREATE TABLE `duvidas` (
 --
 
 CREATE TABLE `imagens_imovel` (
-  `id_imagem` int(11) NOT NULL,
-  `dados_imagem` blob NOT NULL,
-  `data_upload` date NOT NULL
+  `id_imagem` INT(11) NOT NULL,
+  `dados_imagem` BLOB NOT NULL,
+  `data_upload` DATE NOT NULL
+  `imovel_id` INT(11),
+  `origem` ENUM('Item 1',' Item 2','Item 3')
+  FOREIGN KEY (`imovel_id`) REFERENCES `imoveis`(`id_imovel`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -91,23 +99,26 @@ CREATE TABLE `imagens_imovel` (
 --
 
 CREATE TABLE `imoveis` (
-  `id_imovel` int(11) NOT NULL,
-  `tipo_imovel` enum('Casa','Apartamento','Lote','Kitnet/Studio','Sala Comercial') NOT NULL,
-  `operacao` enum('Comprar','Alugar','','') NOT NULL,
-  `descricao` varchar(200) NOT NULL,
-  `num_quartos` int(11) NOT NULL,
-  `num_banheiros` int(11) NOT NULL,
-  `num_vagas` int(11) NOT NULL,
-  `area` float NOT NULL,
-  `valor` decimal(10,0) NOT NULL,
-  `valor_condominio` decimal(10,0) NOT NULL,
-  `iptu` decimal(10,0) NOT NULL,
-  `parcelas_iptu` int(11) NOT NULL,
-  `construcao` tinyint(1) NOT NULL,
-  `num_andares` int(11) NOT NULL,
-  `data_entrega` date NOT NULL,
-  `em_condominio` tinyint(1) NOT NULL,
-  `data_publicacao` date NOT NULL
+  `id_imovel` INT(11) NOT NULL,
+  `tipo_imovel` ENUM('Casa','Apartamento','Lote','Kitnet/Studio','Sala Comercial') NOT NULL,
+  `operacao` ENUM('Comprar','Alugar','Item 3','Item 4') NOT NULL,
+  `descricao` VARCHAR(200) NOT NULL,
+  `num_quartos` INT(11) NOT NULL,
+  `num_banheiros` INT(11) NOT NULL,
+  `num_vagas` INT(11) NOT NULL,
+  `area` FLOAT NOT NULL,
+  `valor` DECIMAL(10,0) NOT NULL,
+  `valor_condominio` DECIMAL(10,0),
+  `iptu` DECIMAL(10,0) NOT NULL,
+  `parcelas_iptu` INT(11) NOT NULL,
+  `construcao` TINYINT(1) NOT NULL,
+  `num_andares` INT(11) NOT NULL,
+  `data_entrega` DATE NOT NULL,
+  `em_condominio` TINYINT(1) NOT NULL,
+  `data_publicacao` DATE NOT NULL,
+  `proprietario_id` INT(11),
+  `imagem_id` INT(11),
+  FOREIGN KEY (`proprietario_id`) REFERENCES `cliente`(`id_cliente`),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -117,13 +128,15 @@ CREATE TABLE `imoveis` (
 --
 
 CREATE TABLE `noticias` (
-  `id_noticia` int(11) NOT NULL,
-  `titulo_noticia` varchar(200) NOT NULL,
-  `descricao_noticia` varchar(200) NOT NULL,
-  `artigo_noticia` text NOT NULL,
-  `autor_noticia` varchar(200) NOT NULL,
-  `publicado_por` varchar(200) NOT NULL DEFAULT 'Lar&House',
-  `imagem_noticia` blob NOT NULL
+  `id_noticia` INT(11) NOT NULL,
+  `titulo_noticia` VARCHAR(200) NOT NULL,
+  `descricao_noticia` VARCHAR(200) NOT NULL,
+  `artigo_noticia` TEXT NOT NULL,
+  `autor_noticia` VARCHAR(200) NOT NULL,
+  `publicado_por` VARCHAR(200) NOT NULL DEFAULT 'Lar&House',
+  `imagem_noticia` BLOB NOT NULL
+  `categoria_id` INT(11),
+  FOREIGN KEY (`categoria_id`) REFERENCES `categoria_noticia`(`id_categoria`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -133,7 +146,14 @@ CREATE TABLE `noticias` (
 --
 
 CREATE TABLE `solicitacoes` (
-  `id_solicitacao` int(11) NOT NULL
+  `id_solicitacao` INT(11) NOT NULL,
+  `status` ENUM('Item 1','Item 2','Item 3')
+  `usuario_id` INT(11),
+  `imovel_id_FK` INT(11),
+  `FK_id_imagem` INT(11),
+  FOREIGN KEY (`imovel_id_FK`) REFERENCES `imoveis`(`id_imovel`),
+  FOREIGN KEY (`usuario_id`) REFERENCES `imoveis`(`id_usuario`),
+  FOREIGN KEY (`FK_id_imagem`) REFERENCES `imagens_imovel`(`id_imagem`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -196,49 +216,49 @@ ALTER TABLE `solicitacoes`
 -- AUTO_INCREMENT de tabela `agenda`
 --
 ALTER TABLE `agenda`
-  MODIFY `id_agenda` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_agenda` INT(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `categoria_noticia`
 --
 ALTER TABLE `categoria_noticia`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_categoria` INT(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_cliente` INT(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `duvidas`
 --
 ALTER TABLE `duvidas`
-  MODIFY `id_duvida` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_duvida` INT(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `imagens_imovel`
 --
 ALTER TABLE `imagens_imovel`
-  MODIFY `id_imagem` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_imagem` INT(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `imoveis`
 --
 ALTER TABLE `imoveis`
-  MODIFY `id_imovel` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_imovel` INT(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `noticias`
 --
 ALTER TABLE `noticias`
-  MODIFY `id_noticia` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_noticia` INT(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `solicitacoes`
 --
 ALTER TABLE `solicitacoes`
-  MODIFY `id_solicitacao` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_solicitacao` INT(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

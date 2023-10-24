aaf7bc03039647ec43d7468e39d31112c11a7fdc
Melanie <melaine.santos@ba.estudante.senai.br>
2023-10-24 14:18:35 -0300

    Adiciona novo layout noticias

--- public/js/noticiasCorretor.js
+++ public/js/noticiasCorretor.js
@@ -1,20 +1,68 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const botaorosio = document.getElementById("pub");
-    const botaoCinza = document.getElementById("des");
-    const publicado = document.getElementById("publicados");
-    const desativado = document.getElementById("desativados");
+// sessions
 
-    botaoCinza.addEventListener("click", function () {
-        botaorosio.classList.remove("ativo");
-        publicado.style.display="none";
-      botaoCinza.classList.add("ativo");
-      desativado.style.display="block";
-    });
-  
-    botaorosio.addEventListener("click", function () {
-      botaoCinza.classList.remove("ativo");
-      desativado.style.display="none";
-      botaorosio.classList.add("ativo");
-      publicado.style.display="block";
-    });
-  });
+openViewNoticia(0)
+
+function openViewNoticia(el){
+    const publicados = document.getElementById("publicados")
+    const desativados = document.getElementById("desativados")
+    const spanPublicados = document.getElementById("totalPublicadas")
+    const spanDesativados = document.getElementById("totalDesativadas")
+    const sectionPublicados = document.getElementById("noticiasPublicadas")
+    const sectionDesativados = document.getElementById("noticiasDesativadas")
+
+    switch(el){
+        case 0:
+            sectionPublicados.style.display = "flex"
+            publicados.style.color = "black"
+            publicados.style.borderBottom = "3px solid #9D0058"
+            spanPublicados.style.backgroundColor = "#FFE9F5"
+            spanPublicados.style.color = "#9D0058"
+        break;
+        case 1:
+            sectionPublicados.style.display = "flex"
+            publicados.style.color = "black"
+            publicados.style.borderBottom = "3px solid #9D0058"
+            spanPublicados.style.backgroundColor = "#FFE9F5"
+            spanPublicados.style.color = "#9D0058"
+
+            sectionDesativados.style.display = "none"
+            desativados.style.color = "#767676"
+            desativados.style.borderBottom = "none"
+            spanDesativados.style.backgroundColor = "#F3F3F3"
+            spanDesativados.style.color = "#6C6C6C"
+        break;
+        case 2:
+            sectionDesativados.style.display = "flex"
+            desativados.style.color = "black"
+            desativados.style.borderBottom = "3px solid #9D0058"
+            spanDesativados.style.backgroundColor = "#FFE9F5"
+            spanDesativados.style.color = "#9D0058"
+
+            sectionPublicados.style.display = "none"
+            publicados.style.color = "#767676"
+            publicados.style.borderBottom = "none"
+            spanPublicados.style.backgroundColor = "#F3F3F3"
+            spanPublicados.style.color = "#6C6C6C"
+        break;
+    }
+
+}
+
+function openBodyDrop(event){
+    console.log(event.target.children)
+    const main = document.getElementsByClassName("main")
+    const dropMore = document.getElementById("moreDrop")
+    const bodyDrop = document.getElementById("bodyDrop")
+
+    bodyDrop.style.display = "flex"
+
+    dropMore.onclick = function(event) {
+        if (bodyDrop.style.display == "flex") {
+          bodyDrop.style.display = "none";
+        }
+        else{
+            bodyDrop.style.display = "flex"
+        }
+      }
+    
+}

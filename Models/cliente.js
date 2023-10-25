99429f68ab662df6db4bdf3ca1e8e6e8d5ec3817
José Saldanha <101759742+Zelusalda@users.noreply.github.com>
2023-10-24 23:51:55 -0300

    Create cliente.js
    Criando para o login funcionar

---
+++ Models/cliente.js
@@ -0,0 +1,50 @@
+const db = require('../Database/Connection.js');
+const Sequelize = require('sequelize');
+const Cliente = db.connect.define('cliente', {
+    id_cliente: {
+        type: db.Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nome_cliente: {
+        type: db.Sequelize.STRING,
+        allowNull: false
+    },
+    email_cliente: {
+        type: db.Sequelize.STRING,
+        allowNull: false
+    },
+    telefone_cliente: {
+        type: db.Sequelize.STRING,
+        allowNull: false
+    },
+    senha_cliente: {
+        type: db.Sequelize.STRING,
+        allowNull: false
+    },
+    createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+});
+
+db.connect.sync({ force: true }).then(() => {
+    console.log('Tabelas sincronizadas.');
+});
+
+Cliente.validarCredenciais = async function(email, senha) {
+    const cliente = await Cliente.findOne({ where: { email_cliente: email } });
+    if (cliente && cliente.senha_cliente === senha) {
+        return cliente;
+    }
+    return null;
+};
+
+
+module.exports = Cliente;

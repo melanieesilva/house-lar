0182b2f0fd10ea541e3f3f7f8120f84a4ca8a58e
Isabel Costa <125329797+zabeldsc@users.noreply.github.com>
2023-10-30 21:12:27 -0300

    ViewImovel feita

--- models/Imoveis.js
+++ models/Imoveis.js
@@ -4,8 +4,9 @@ const db = require('../Database/Connection')
 //Definindo modelo Imoveis
 
 const Imovel = db.connect.define('Imoveis',{
+    statusImovel: db.Sequelize.ENUM('Publicado','Desativado'),
     tipo_imovel: db.Sequelize.ENUM('Casa','Apartamento','Lote','Kitnet/Studio','Sala Comercial'),
-    operacao: db.Sequelize.ENUM('Comprar','Alugar'),
+    operacao: db.Sequelize.ENUM('Venda','Aluguel'),
     descricao: db.Sequelize.STRING,
     num_quartos: db.Sequelize.INTEGER,
     num_banheiros: db.Sequelize.INTEGER,
@@ -16,6 +17,8 @@ const Imovel = db.connect.define('Imoveis',{
     iptu: db.Sequelize.DECIMAL,
     parcelas_iptu: db.Sequelize.INTEGER,
     construcao: db.Sequelize.TINYINT,
+    cidade: db.Sequelize.STRING,
+	bairro: db.Sequelize.STRING,
     num_andares: db.Sequelize.INTEGER,
     data_entrega: db.Sequelize.DATE,
     em_condominio: db.Sequelize.TINYINT,
